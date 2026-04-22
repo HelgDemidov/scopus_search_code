7ec701c4bce5299852ae6cfbdb69695f0502e778
HelgDemidov <helg.demidov88@gmail.com>
2026-04-22 19:19:25 +0200

    fixes in integration tests (tests/integration/test_articles_headers.py & tests/integration/test_find_articles.py)

--- tests/integration/test_find_articles.py
+++ tests/integration/test_find_articles.py
@@ -15,7 +15,6 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.models.article import Article
 from app.models.search_history import SearchHistory
 from app.main import app
-from app.routers.articles import get_scopus_client
 from app.interfaces.search_client import ISearchClient
 from app.services.search_service import SearchService
 
